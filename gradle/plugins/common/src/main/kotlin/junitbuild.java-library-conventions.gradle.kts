9ee05594d67f59e3622838009a76b2231101f788
Vincent Potucek <vpotucek@me.com>
2025-07-10 22:04:09 +0200

    (re)activate rewrite capability

--- gradle/plugins/common/src/main/kotlin/junitbuild.java-library-conventions.gradle.kts
+++ gradle/plugins/common/src/main/kotlin/junitbuild.java-library-conventions.gradle.kts
@@ -1,10 +1,5 @@
 import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
-import com.gradle.scan.agent.serialization.scan.serializer.kryo.it
-import groovy.xml.dom.DOMCategory.attributes
-import jdk.javadoc.internal.doclets.formats.html.markup.HtmlStyle
-import junitbuild.extensions.dependencyFromLibs
 import junitbuild.extensions.isSnapshot
-import org.gradle.internal.impldep.org.apache.http.client.methods.RequestBuilder.options
 import org.gradle.plugins.ide.eclipse.model.Classpath
 import org.gradle.plugins.ide.eclipse.model.Library
 import org.gradle.plugins.ide.eclipse.model.ProjectDependency
@@ -94,7 +89,7 @@ if (project in mavenizedProjects) {
 	tasks.javadoc {
 		options {
 			memberLevel = JavadocMemberLevel.PROTECTED
-			HtmlStyle.header = project.name
+			header = project.name
 			encoding = "UTF-8"
 			locale = "en"
 			(this as StandardJavadocDocletOptions).apply {
