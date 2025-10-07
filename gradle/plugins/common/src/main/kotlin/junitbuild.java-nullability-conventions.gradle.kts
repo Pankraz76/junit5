6fc55a89d0f85751e3224ca07c05cbae09201476
Vincent Potucek <vpotucek@me.com>
2025-10-07 18:31:49 +0200

    Add `error-prone.picnic.tech`

--- gradle/plugins/common/src/main/kotlin/junitbuild.java-nullability-conventions.gradle.kts
+++ gradle/plugins/common/src/main/kotlin/junitbuild.java-nullability-conventions.gradle.kts
@@ -14,14 +14,14 @@ dependencies {
 	errorprone(dependencyFromLibs("error-prone-core"))
 	errorprone(dependencyFromLibs("nullaway"))
 	errorprone(dependencyFromLibs("refaster-runner"))
-	constraints {
-		errorprone("com.google.guava:guava") {
-			version {
-				require("33.4.8-jre")
-			}
-			because("Older versions use deprecated methods in sun.misc.Unsafe")
-		}
-	}
+//	constraints {
+//		errorprone("com.google.guava:guava") {
+//			version {
+//				require("33.4.8-jre")
+//			}
+//			because("Older versions use deprecated methods in sun.misc.Unsafe")
+//		}
+//	}
 }
 
 nullaway {
@@ -32,46 +32,47 @@ tasks.withType<JavaCompile>().configureEach {
 	options.errorprone {
 		disableWarningsInGeneratedCode = true
 		allErrorsAsWarnings = true
+		allDisabledChecksAsWarnings = true
 		errorproneArgs.add("-XepOpt:Refaster:NamePattern=^(?!.*Rules\\$).*") // might consider.
-		if (true) {
-		//if (!getenv().containsKey("CI") && getenv("IN_PLACE").toBoolean()) {
-			errorproneArgs.addAll(
-				"-XepPatchLocation:IN_PLACE",
-				"-XepPatchChecks:" +
-						"ConstantNaming," +
-						"EmptyMethod," +
-						"EmptyMonoZip," +
-						"LexicographicalAnnotationAttributeListing," +
-						"LexicographicalAnnotationListing," +
-						"MissingOverride," +
-						"MissingTestCall," +
-						"NonEmptyMono," +
-						"OptionalMapUnusedValue," +
-						"OptionalOfRedundantMethod," +
-						"RedundantSetterCall," +
-						"RedundantStringConversion," +
-						"RedundantStringEscape," +
-						"StaticImport," +
-						"StringJoin," +
-						"UnnecessaryCheckNotNull," +
-						"UnnecessaryTypeArgument," +
-						"UnusedAnonymousClass," +
-						"UnusedCollectionModifiedInPlace,"
-			)
-		}
 		val shouldDisableErrorProne = java.toolchain.implementation.orNull == JvmImplementation.J9
 		if (name == "compileJava" && !shouldDisableErrorProne) {
-			disable(
-				"AnnotateFormatMethod", // We don`t want to use ErrorProne`s annotations.
-				"BadImport", // This check is opinionated wrt. which method names it considers unsuitable for import which includes a few of our own methods in `ReflectionUtils` etc.
-				"DirectReturn", // https://github.com/junit-team/junit-framework/pull/5006#discussion_r2403984446
-				"DoNotCallSuggester",
-				"ImmutableEnumChecker",
-				"InlineMeSuggester",
-				"MissingSummary", // Produces a lot of findings that we consider to be false positives, for example for package-private classes and methods.
-				"StringSplitter", // We don`t want to use Guava.
-				"UnnecessaryLambda", // The findings of this check are subjective because a named constant can be more readable in many cases.
-			)
+			if (true) {
+				//if (!getenv().containsKey("CI") && getenv("IN_PLACE").toBoolean()) {
+				errorproneArgs.addAll(
+					"-XepPatchLocation:IN_PLACE",
+					"-XepPatchChecks:" +
+							"ConstantNaming," +
+							"EmptyMethod," +
+							"EmptyMonoZip," +
+							"LexicographicalAnnotationAttributeListing," +
+							"LexicographicalAnnotationListing," +
+							"MissingOverride," +
+							"MissingTestCall," +
+							"NonEmptyMono," +
+							"OptionalMapUnusedValue," +
+							"OptionalOfRedundantMethod," +
+							"RedundantSetterCall," +
+							"RedundantStringConversion," +
+							"RedundantStringEscape," +
+							"StaticImport," +
+							"StringJoin," +
+							"UnnecessaryCheckNotNull," +
+							"UnnecessaryTypeArgument," +
+							"UnusedAnonymousClass," +
+							"UnusedCollectionModifiedInPlace,"
+				)
+			}
+//			disable(
+//				"AnnotateFormatMethod", // We don`t want to use ErrorProne`s annotations.
+//				"BadImport", // This check is opinionated wrt. which method names it considers unsuitable for import which includes a few of our own methods in `ReflectionUtils` etc.
+//				"DirectReturn", // https://github.com/junit-team/junit-framework/pull/5006#discussion_r2403984446
+//				"DoNotCallSuggester",
+//				"ImmutableEnumChecker",
+//				"InlineMeSuggester",
+//				"MissingSummary", // Produces a lot of findings that we consider to be false positives, for example for package-private classes and methods.
+//				"StringSplitter", // We don`t want to use Guava.
+//				"UnnecessaryLambda", // The findings of this check are subjective because a named constant can be more readable in many cases.
+//			)
 //			error(
 //				"ConstantNaming",
 //				"EmptyMethod",
@@ -94,29 +95,29 @@ tasks.withType<JavaCompile>().configureEach {
 //				"UnusedAnonymousClass",
 //				"UnusedCollectionModifiedInPlace",
 //			)
-			enable(
-				"ConstantNaming",
-				"EmptyMethod",
-				"EmptyMonoZip",
-				"LexicographicalAnnotationAttributeListing",
-				"LexicographicalAnnotationListing",
-				"MissingOverride",
-				"MissingTestCall",
-				"NonEmptyMono",
-				"OptionalMapUnusedValue",
-				"OptionalOfRedundantMethod",
-				"RedundantSetterCall",
-				"RedundantStringConversion",
-				"RedundantStringEscape",
-				"StaticImport",
-				"StringJoin",
-				"UnnecessaryCheckNotNull",
-				"UnnecessaryTypeArgument",
-				"UnusedAnonymousClass",
-				"UnusedCollectionModifiedInPlace",
-			)
+//			enable(
+//				"ConstantNaming",
+//				"EmptyMethod",
+//				"EmptyMonoZip",
+//				"LexicographicalAnnotationAttributeListing",
+//				"LexicographicalAnnotationListing",
+//				"MissingOverride",
+//				"MissingTestCall",
+//				"NonEmptyMono",
+//				"OptionalMapUnusedValue",
+//				"OptionalOfRedundantMethod",
+//				"RedundantSetterCall",
+//				"RedundantStringConversion",
+//				"RedundantStringEscape",
+//				"StaticImport",
+//				"StringJoin",
+//				"UnnecessaryCheckNotNull",
+//				"UnnecessaryTypeArgument",
+//				"UnusedAnonymousClass",
+//				"UnusedCollectionModifiedInPlace",
+//			)
 		} else {
-			disableAllChecks = true
+//			disableAllChecks = true
 		}
 		nullaway {
 			if (shouldDisableErrorProne) {
