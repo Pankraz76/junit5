d29824a831d2c3416682bb4f471a85ef9ef7d2b0
Vincent Potucek <vpotucek@me.com>
2025-10-07 19:56:13 +0200

    Add `error-prone.picnic.tech` featuring `LexicographicalAnnotation`

--- gradle/plugins/common/src/main/kotlin/junitbuild.java-nullability-conventions.gradle.kts
+++ gradle/plugins/common/src/main/kotlin/junitbuild.java-nullability-conventions.gradle.kts
@@ -32,9 +32,9 @@ tasks.withType<JavaCompile>().configureEach {
 				"AnnotateFormatMethod", // We don`t want to use ErrorProne`s annotations.
 				"BadImport", // This check is opinionated wrt. which method names it considers unsuitable for import which includes a few of our own methods in `ReflectionUtils` etc.
 				"DirectReturn", // https://github.com/junit-team/junit-framework/pull/5006#discussion_r2403984446
-				"DoNotCallSuggester",
-				"ImmutableEnumChecker",
-				"InlineMeSuggester",
+//				"DoNotCallSuggester",
+//				"ImmutableEnumChecker",
+//				"InlineMeSuggester",
 				"MissingSummary", // Produces a lot of findings that we consider to be false positives, for example for package-private classes and methods.
 				"StringSplitter", // We don`t want to use Guava.
 				//"UnnecessaryLambda", // The findings of this check are subjective because a named constant can be more readable in many cases.
