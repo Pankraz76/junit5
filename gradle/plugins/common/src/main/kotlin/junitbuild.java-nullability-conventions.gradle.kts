938376cd2bdafe2b283866fe76d13692313fcc53
Vincent Potucek <vpotucek@me.com>
2025-10-07 19:57:50 +0200

    Add `error-prone.picnic.tech` featuring `LexicographicalAnnotation`

--- gradle/plugins/common/src/main/kotlin/junitbuild.java-nullability-conventions.gradle.kts
+++ gradle/plugins/common/src/main/kotlin/junitbuild.java-nullability-conventions.gradle.kts
@@ -24,7 +24,7 @@ tasks.withType<JavaCompile>().configureEach {
 	options.errorprone {
 		allDisabledChecksAsWarnings = true
 		allErrorsAsWarnings = true // without prone breaks build, but does not apply fixes.
-		disableAllChecks = java.toolchain.implementation.orNull == JvmImplementation.J9 && name == "compileJava"
+		disableAllChecks = java.toolchain.implementation.orNull != JvmImplementation.J9 && name == "compileJava"
 		disableWarningsInGeneratedCode = true
 		errorproneArgs.add("-XepOpt:Refaster:NamePattern=^(?!.*Rules\\$).*") // currently failing Refaster; might consider whitelist.
 		if (!disableAllChecks.get()) {
