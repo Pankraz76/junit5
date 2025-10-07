23d3522c14f7347337a5ff8c54565b4991ba7719
Vincent Potucek <vpotucek@me.com>
2025-10-07 19:22:34 +0200

    Add `error-prone.picnic.tech`

--- gradle/plugins/common/src/main/kotlin/junitbuild.java-nullability-conventions.gradle.kts
+++ gradle/plugins/common/src/main/kotlin/junitbuild.java-nullability-conventions.gradle.kts
@@ -4,16 +4,16 @@ import net.ltgt.gradle.nullaway.nullaway
 import java.lang.System.getenv
 
 plugins {
-	`java-library`
-	id("net.ltgt.errorprone")
-	id("net.ltgt.nullaway")
+    `java-library`
+    id("net.ltgt.errorprone")
+    id("net.ltgt.nullaway")
 }
 
 dependencies {
-	errorprone(dependencyFromLibs("error-prone-contrib"))
-	errorprone(dependencyFromLibs("error-prone-core"))
-	errorprone(dependencyFromLibs("nullaway"))
-	errorprone(dependencyFromLibs("refaster-runner"))
+    errorprone(dependencyFromLibs("error-prone-contrib"))
+    errorprone(dependencyFromLibs("error-prone-core"))
+    errorprone(dependencyFromLibs("nullaway"))
+    errorprone(dependencyFromLibs("refaster-runner"))
 //	constraints {
 //		errorprone("com.google.guava:guava") {
 //			version {
@@ -25,79 +25,79 @@ dependencies {
 }
 
 nullaway {
-	onlyNullMarked = true
+    onlyNullMarked = true
 }
 
 tasks.withType<JavaCompile>().configureEach {
-	options.errorprone {
-		allDisabledChecksAsWarnings = true
-		allErrorsAsWarnings = true
-		disableAllChecks = java.toolchain.implementation.orNull == JvmImplementation.J9 && name == "compileJava"
-		disableWarningsInGeneratedCode = true
-		errorproneArgs.add("-XepOpt:Refaster:NamePattern=^(?!.*Rules\\$).*") // currently failing Refaster; might consider whitelist.
-		if (!disableAllChecks.get()) {
-			disable(
-				"AnnotateFormatMethod", // We don`t want to use ErrorProne`s annotations.
-				"BadImport", // This check is opinionated wrt. which method names it considers unsuitable for import which includes a few of our own methods in `ReflectionUtils` etc.
-				"DirectReturn", // https://github.com/junit-team/junit-framework/pull/5006#discussion_r2403984446
-				"DoNotCallSuggester",
-				"ImmutableEnumChecker",
-				"InlineMeSuggester",
-				"MissingSummary", // Produces a lot of findings that we consider to be false positives, for example for package-private classes and methods.
-				"StringSplitter", // We don`t want to use Guava.
-				"UnnecessaryLambda", // The findings of this check are subjective because a named constant can be more readable in many cases.
-			)
-			error(
-				"MissingOverride",
-				"PackageLocation",
-				"RedundantStringConversion",
-				"RedundantStringEscape",
-				"UnusedVariable",
-				// why not working ?
-				//"LexicographicalAnnotationAttributeListing",
-				//"LexicographicalAnnotationListing",
-				//"StaticImport",
-			)
-			if (!getenv().containsKey("CI") && getenv("IN_PLACE").toBoolean()) {
-				errorproneArgs.addAll(
-					"-XepPatchLocation:IN_PLACE",
-					"-XepPatchChecks:" +
-							"MissingOverride," +
-							"PackageLocation," +
-							"UnusedVariable," +
-							"RedundantStringConversion," +
-							"StaticImport," +
-							"LexicographicalAnnotationListing," +
-							"LexicographicalAnnotationAttributeListing," +
-							"RedundantStringEscape"
-				)
-			}
-		}
-		nullaway {
-			if (disableAllChecks.get()) {
-				disable()
-			} else {
-				enable()
-			}
-			checkContracts = true
-			customContractAnnotations.add("org.junit.platform.commons.annotation.Contract")
-			isJSpecifyMode = true
-			suppressionNameAliases.add("DataFlowIssue")
-		}
-	}
+    options.errorprone {
+        allDisabledChecksAsWarnings = true
+        allErrorsAsWarnings = true
+        disableAllChecks = java.toolchain.implementation.orNull == JvmImplementation.J9 && name == "compileJava"
+        disableWarningsInGeneratedCode = true
+        errorproneArgs.add("-XepOpt:Refaster:NamePattern=^(?!.*Rules\\$).*") // currently failing Refaster; might consider whitelist.
+        if (!disableAllChecks.get()) {
+            disable(
+                "AnnotateFormatMethod", // We don`t want to use ErrorProne`s annotations.
+                "BadImport", // This check is opinionated wrt. which method names it considers unsuitable for import which includes a few of our own methods in `ReflectionUtils` etc.
+                "DirectReturn", // https://github.com/junit-team/junit-framework/pull/5006#discussion_r2403984446
+                "DoNotCallSuggester",
+                "ImmutableEnumChecker",
+                "InlineMeSuggester",
+                "MissingSummary", // Produces a lot of findings that we consider to be false positives, for example for package-private classes and methods.
+                "StringSplitter", // We don`t want to use Guava.
+                "UnnecessaryLambda", // The findings of this check are subjective because a named constant can be more readable in many cases.
+            )
+            error(
+                "MissingOverride",
+                "PackageLocation",
+                "RedundantStringConversion",
+                "RedundantStringEscape",
+                "UnusedVariable",
+                // why not working ?
+                //"LexicographicalAnnotationAttributeListing",
+                //"LexicographicalAnnotationListing",
+                //"StaticImport",
+            )
+            if (!getenv().containsKey("CI") && getenv("IN_PLACE").toBoolean()) {
+                errorproneArgs.addAll(
+                    "-XepPatchLocation:IN_PLACE",
+                    "-XepPatchChecks:" +
+                            "LexicographicalAnnotationAttributeListing," +
+                            "LexicographicalAnnotationListing," +
+                            "MissingOverride," +
+                            "PackageLocation," +
+                            "RedundantStringConversion," +
+                            "RedundantStringEscape" +
+                            "StaticImport," +
+                            "UnusedVariable,"
+                )
+            }
+        }
+        nullaway {
+            if (disableAllChecks.get()) {
+                disable()
+            } else {
+                enable()
+            }
+            checkContracts = true
+            customContractAnnotations.add("org.junit.platform.commons.annotation.Contract")
+            isJSpecifyMode = true
+            suppressionNameAliases.add("DataFlowIssue")
+        }
+    }
 }
 
 tasks.withType<JavaCompile>().named { it.startsWith("compileTest") }.configureEach {
-	options.errorprone.nullaway {
-		handleTestAssertionLibraries = true
-		excludedFieldAnnotations.addAll(
-			"org.junit.jupiter.api.io.TempDir",
-			"org.junit.jupiter.params.Parameter",
-			"org.junit.runners.Parameterized.Parameter",
-			"org.mockito.Captor",
-			"org.mockito.InjectMocks",
-			"org.mockito.Mock",
-			"org.mockito.Spy",
-		)
-	}
+    options.errorprone.nullaway {
+        handleTestAssertionLibraries = true
+        excludedFieldAnnotations.addAll(
+            "org.junit.jupiter.api.io.TempDir",
+            "org.junit.jupiter.params.Parameter",
+            "org.junit.runners.Parameterized.Parameter",
+            "org.mockito.Captor",
+            "org.mockito.InjectMocks",
+            "org.mockito.Mock",
+            "org.mockito.Spy",
+        )
+    }
 }
