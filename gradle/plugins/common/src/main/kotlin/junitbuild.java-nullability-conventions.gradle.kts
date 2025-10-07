20542f60ba887f52541eccc0e28bce59650fd249
Vincent Potucek <vpotucek@me.com>
2025-10-07 19:00:26 +0200

    Add `error-prone.picnic.tech`

--- gradle/plugins/common/src/main/kotlin/junitbuild.java-nullability-conventions.gradle.kts
+++ gradle/plugins/common/src/main/kotlin/junitbuild.java-nullability-conventions.gradle.kts
@@ -40,62 +40,24 @@ tasks.withType<JavaCompile>().configureEach {
 				//if (!getenv().containsKey("CI") && getenv("IN_PLACE").toBoolean()) {
 				errorproneArgs.addAll(
 					"-XepPatchLocation:IN_PLACE",
-					"-XepPatchChecks:StaticImport"
+					"-XepPatchChecks:StaticImport,PackageLocation"
 				)
 			}
-//			disable(
-//				"AnnotateFormatMethod", // We don`t want to use ErrorProne`s annotations.
-//				"BadImport", // This check is opinionated wrt. which method names it considers unsuitable for import which includes a few of our own methods in `ReflectionUtils` etc.
-//				"DirectReturn", // https://github.com/junit-team/junit-framework/pull/5006#discussion_r2403984446
-//				"DoNotCallSuggester",
-//				"ImmutableEnumChecker",
-//				"InlineMeSuggester",
-//				"MissingSummary", // Produces a lot of findings that we consider to be false positives, for example for package-private classes and methods.
-//				"StringSplitter", // We don`t want to use Guava.
-//				"UnnecessaryLambda", // The findings of this check are subjective because a named constant can be more readable in many cases.
-//			)
-			error(
-				"ConstantNaming",
-				"EmptyMethod",
-				"EmptyMonoZip",
-				"LexicographicalAnnotationAttributeListing",
-				"LexicographicalAnnotationListing",
-				"MissingOverride",
-				"MissingTestCall",
-				"NonEmptyMono",
-				"OptionalMapUnusedValue",
-				"OptionalOfRedundantMethod",
-				"PackageLocation",
-				"RedundantSetterCall",
-				"RedundantStringConversion",
-				"RedundantStringEscape",
-				"StaticImport",
-				"StringJoin",
-				"UnnecessaryCheckNotNull",
-				"UnnecessaryTypeArgument",
-				"UnusedAnonymousClass",
-				"UnusedCollectionModifiedInPlace",
+			disable(
+				"AnnotateFormatMethod", // We don`t want to use ErrorProne`s annotations.
+				"BadImport", // This check is opinionated wrt. which method names it considers unsuitable for import which includes a few of our own methods in `ReflectionUtils` etc.
+				"DirectReturn", // https://github.com/junit-team/junit-framework/pull/5006#discussion_r2403984446
+				"DoNotCallSuggester",
+				"ImmutableEnumChecker",
+				"InlineMeSuggester",
+				"MissingSummary", // Produces a lot of findings that we consider to be false positives, for example for package-private classes and methods.
+				"StringSplitter", // We don`t want to use Guava.
+				"UnnecessaryLambda", // The findings of this check are subjective because a named constant can be more readable in many cases.
 			)
-//			enable(
-//				"ConstantNaming",
-//				"EmptyMethod",
-//				"EmptyMonoZip",
-//				"LexicographicalAnnotationAttributeListing",
-//				"LexicographicalAnnotationListing",
-//				"MissingOverride",
-//				"MissingTestCall",
-//				"NonEmptyMono",
-//				"OptionalMapUnusedValue",
-//				"OptionalOfRedundantMethod",
-//				"RedundantSetterCall",
-//				"RedundantStringConversion",
-//				"RedundantStringEscape",
-//				"StaticImport",
-//				"StringJoin",
-//				"UnnecessaryCheckNotNull",
-//				"UnnecessaryTypeArgument",
-//				"UnusedAnonymousClass",
-//				"UnusedCollectionModifiedInPlace",
+			error(
+				"PackageLocation",
+				"StaticImport",
+			)
 //			)
 		} else {
 //			disableAllChecks = true
