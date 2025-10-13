1063171b065d9e8b4c990af68ac2a17861503b6d
Vincent Potucek <vpotucek@me.com>
2025-10-13 11:32:19 +0200

    Disable `LexicographicalAnnotationAttributeListing`

--- gradle/plugins/common/src/main/kotlin/junitbuild.java-nullability-conventions.gradle.kts
+++ gradle/plugins/common/src/main/kotlin/junitbuild.java-nullability-conventions.gradle.kts
@@ -37,7 +37,7 @@ tasks.withType<JavaCompile>().configureEach {
 				"StringSplitter", // We don`t want to use Guava.
 				"UnnecessaryLambda", // The findings of this check are subjective because a named constant can be more readable in many cases.
 				// picnic (https://error-prone.picnic.tech)
-				"ConstantNaming",
+				//"ConstantNaming",
 				"DirectReturn", // We don`t want to use this: https://github.com/junit-team/junit-framework/pull/5006#discussion_r2403984446
 				"FormatStringConcatenation",
 				"IdentityConversion",
@@ -58,7 +58,7 @@ tasks.withType<JavaCompile>().configureEach {
 			)
 			errorproneArgs.addAll(
 				"-XepPatchLocation:IN_PLACE",
-				"-XepPatchChecks:RedundantStringConversion"
+				"-XepPatchChecks:ConstantNaming"
 			)
 		} else {
 			disableAllChecks = true
