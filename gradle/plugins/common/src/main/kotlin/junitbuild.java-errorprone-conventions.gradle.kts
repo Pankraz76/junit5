0c0d86cfa03050e217c06269914688bf840f9fb6
Vincent Potucek <vpotucek@me.com>
2025-10-17 14:09:15 +0200

    [prone] Add `Unused`

--- gradle/plugins/common/src/main/kotlin/junitbuild.java-errorprone-conventions.gradle.kts
+++ gradle/plugins/common/src/main/kotlin/junitbuild.java-errorprone-conventions.gradle.kts
@@ -26,26 +26,64 @@ dependencies {
 
 tasks.withType<JavaCompile>().configureEach {
 	options.errorprone {
-		disableAllChecks = true // remove once applied error-prone.
-		allErrorsAsWarnings = true
-		error(
-			// "ConstantNaming", # https://github.com/PicnicSupermarket/error-prone-support/issues/1923
-			"Unused",
-			"UnusedMethod",
-			"UnusedVariable",
-			"UnusedParameters",
-			"RedundantStringConversion",
-		)
-		if (!getenv().containsKey("CI") && getenv("IN_PLACEe").toBoolean()) {
-			errorproneArgs.addAll(
-				"-XepPatchLocation:IN_PLACE",
-				"-XepPatchChecks:" +
-						"RedundantStringConversion," +
-						"Unused," +
-						"UnusedMethod," +
-						"UnusedParameters," +
-						"UnusedVariable,"
+		val shouldDisableErrorProne = java.toolchain.implementation.orNull == JvmImplementation.J9
+		if (name == "compileJava" && !shouldDisableErrorProne) {
+			disable(
+				"AnnotateFormatMethod", // We don`t want to use ErrorProne's annotations.
+				"BadImport", // This check is opinionated wrt. which method names it considers unsuitable for import which includes a few of our own methods in `ReflectionUtils` etc.
+				"DoNotCallSuggester", // We don`t want to use ErrorProne's annotations.
+				"ImmutableEnumChecker", // We don`t want to use ErrorProne's annotations.
+				"InlineMeSuggester", // We don`t want to use ErrorProne's annotations.
+				"MissingSummary", // Produces a lot of findings that we consider to be false positives, for example for package-private classes and methods.
+				"StringSplitter", // We don`t want to use Guava.
+				"UnnecessaryLambda", // The findings of this check are subjective because a named constant can be more readable in many cases.
+				// picnic (https://error-prone.picnic.tech)
+				"ConstantNaming",
+				"DirectReturn", // We don`t want to use this: https://github.com/junit-team/junit-framework/pull/5006#discussion_r2403984446
+				"FormatStringConcatenation",
+				"IdentityConversion",
+				"LexicographicalAnnotationAttributeListing", // We don`t want to use this: https://github.com/junit-team/junit-framework/pull/5043#pullrequestreview-3330615838
+				"LexicographicalAnnotationListing",
+				"MissingTestCall",
+				"NestedOptionals",
+				"NonStaticImport",
+				"OptionalOrElseGet",
+				"PrimitiveComparison",
+				"StaticImport",
+				"TimeZoneUsage",
 			)
+			error(
+				"CanonicalAnnotationSyntax",
+				"IsInstanceLambdaUsage",
+				"PackageLocation",
+				"RedundantStringConversion",
+				"RedundantStringEscape",
+			)
+			if (!getenv().containsKey("CI") && getenv("IN_PLACEe").toBoolean()) {
+				errorproneArgs.addAll(
+					"-XepPatchLocation:IN_PLACE",
+					"-XepPatchChecks:" +
+							"RedundantStringConversion," +
+							"Unused," +
+							"UnusedMethod," +
+							"UnusedParameters," +
+							"UnusedVariable,"
+				)
+			}
+		} else {
+			disableAllChecks = true
+		}
+		nullaway {
+			if (shouldDisableErrorProne) {
+				disable()
+			} else {
+				enable()
+			}
+			onlyNullMarked = true
+			isJSpecifyMode = true
+			customContractAnnotations.add("org.junit.platform.commons.annotation.Contract")
+			checkContracts = true
+			suppressionNameAliases.add("DataFlowIssue")
 		}
 	}
 }
