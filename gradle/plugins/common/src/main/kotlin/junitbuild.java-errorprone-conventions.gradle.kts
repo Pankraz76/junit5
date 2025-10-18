5ea8ef02472b8e57a2a77880c0a3d87c1f533bba
Vincent Potucek <vpotucek@me.com>
2025-10-18 13:01:03 +0200

    [prone] Add `Unused`

--- gradle/plugins/common/src/main/kotlin/junitbuild.java-errorprone-conventions.gradle.kts
+++ gradle/plugins/common/src/main/kotlin/junitbuild.java-errorprone-conventions.gradle.kts
@@ -45,7 +45,7 @@ tasks.withType<JavaCompile>().configureEach {
 				"LexicographicalAnnotationAttributeListing", // We don`t want to use this: https://github.com/junit-team/junit-framework/pull/5043#pullrequestreview-3330615838
 				"LexicographicalAnnotationListing",
 				"MissingTestCall",
-				"NestedOptionals",
+//				"NestedOptionals",
 				"NonStaticImport",
 				"OptionalOrElseGet",
 				"PrimitiveComparison",
