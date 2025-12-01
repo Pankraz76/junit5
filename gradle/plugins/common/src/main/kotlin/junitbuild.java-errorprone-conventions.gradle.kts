2c687bf57706356b28a6211d97fb47ccd74e0206
Vincent Potucek <vpotucek@me.com>
2025-12-01 20:15:23 +0100

    [prone] fix `UnnecessarilyFullyQualified`
    - https://errorprone.info/bugpattern/UnnecessarilyFullyQualified
    
    Signed-off-by: Vincent Potucek <vpotucek@me.com>

--- gradle/plugins/common/src/main/kotlin/junitbuild.java-errorprone-conventions.gradle.kts
+++ gradle/plugins/common/src/main/kotlin/junitbuild.java-errorprone-conventions.gradle.kts
@@ -33,6 +33,7 @@ tasks.withType<JavaCompile>().configureEach {
 				"StringSplitter", // We don`t want to use Guava.
 				"UnnecessaryLambda", // The findings of this check are subjective because a named constant can be more readable in many cases.
 				// picnic (https://error-prone.picnic.tech)
+				"ConstantNaming",
 				"DirectReturn", // We don`t want to use this: https://github.com/junit-team/junit-framework/pull/5006#discussion_r2403984446
 				"FormatStringConcatenation",
 				"IdentityConversion",
@@ -50,7 +51,6 @@ tasks.withType<JavaCompile>().configureEach {
 				"CanonicalAnnotationSyntax",
 				"IsInstanceLambdaUsage",
 				"MissingOverride",
-				"ConstantNaming",
 				"PackageLocation",
 				"RedundantStringConversion",
 				"RedundantStringEscape",
@@ -60,14 +60,6 @@ tasks.withType<JavaCompile>().configureEach {
 				"UnnecessarilyFullyQualified",
 			)
 			excludedPaths.set(".*module-info.java") // incompatible with UnnecessarilyFullyQualified
-			if (!getenv().containsKey("CI") && getenv("IN_PLACE").toBoolean()) {
-				errorproneArgs.addAll(
-					"-XepPatchLocation:IN_PLACE",
-					"-XepPatchChecks:" +
-							"ConstantNaming," +
-							"UnnecessarilyFullyQualified"
-				)
-			}
 		} else {
 			disableAllChecks = true
 		}
